9d5cee0f9ad8a3f798e990f02ba37fd79035b714
jbd16 <janer27@gmail.com>
2016-04-24 21:22:29 -0700

    fixed carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+  	@products = Product.limit(3)
   end
 
   def landing_page
   	@featured_product = Product.first
+  	@products = Product.limit(3)
   end
+
   
 end
