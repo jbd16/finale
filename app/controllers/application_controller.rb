49ebb839d2f5b5d79460c67192cb01afc3314c0f
jbd16 <janer27@gmail.com>
2016-04-30 15:34:16 -0700

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  
+ before_action :set_user, only: [:show, :edit, :update, :destroy]
+ before_filter :authenticate_user!
+ load_and_authorize_resource
+
+rescue_from CanCan::AccessDenied do |exception|
+	redirect_to main_app.root_url, :alert => exception.message
+end
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
