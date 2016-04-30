d2681d0a41bd013ae241cd194dd212a8a020e522
jbd16 <janer27@gmail.com>
2016-04-29 22:47:58 -0700

    adding search bar and adjusting navigation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,14 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
-  end
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name LIKE ?", "%#{search_term}")
+      # return our filtered list here
+    else
+      @products = Product.all
+    end
+  end  
 
   # GET /products/1
   # GET /products/1.json
@@ -28,7 +34,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "/static_pages/landing_page", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
