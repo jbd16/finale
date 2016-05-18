8eaf4ba85cd0655826a8cd961ed946dcf2db7992
jbd16 <janer27@gmail.com>
2016-05-17 17:10:48 -0700

    3.9 additions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,22 @@ class CommentsController < ApplicationController
 def create
 	@product = Product.find(params[:product_id])
 	@comment = @product.comments.new(params[:comment])
-	@comment.user = current user
-	@comment.save
+	@comment.user = current_user
+	respond_to do |format|
+		if @comment.save
+			format.html { redirect_to @product, notice: 'Review
+				was created successfully.'}
+			format.json { render :show, status: :created, location: @product}
+
+		else
+			format.html { redirect_to @product, alert: 'Review was not saved
+				successfully.'}
+			format.json { render json: @comment.errors, status: :unprocessable_entity}
+		end
+	end
+end
 	redirect_to product_path(@product)
+	@posts = Post.paginate(:page => params[:page])
 end
 
 def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,9 @@ class Product < ActiveRecord::Base
 		comments.averages(:rating).to_f
 	end
 
+	validates :name, presence: true
+	  validates :user, presence: true
+	  validates :product, presence: true
+	  validates :rating, numericality: { only_integer: true}
+
 end
