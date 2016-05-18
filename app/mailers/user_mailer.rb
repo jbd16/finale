02cb6fe741c6cb4965040335cb1f0a93a3d6e6d5
jbd16 <janer27@gmail.com>
2016-05-17 18:09:21 -0700

    added email elements

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,12 @@ def contact_form(email, name, message)
 		:to => 'your-email@example.com',
 		:subject => "A new contact form message from #{name}")
 end
+
+def welcome(user)
+	@appname = "Bike Shop"
+	mail( :to => user.email,
+		  :subject => "Welcome to #{@appname}!")
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user_registrations"}
   resources :products do
     resources :comments
   end
